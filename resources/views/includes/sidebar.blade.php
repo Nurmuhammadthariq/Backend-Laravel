f10fb369a6451098912f8eb102c0fe45de349414
Nurmuhammadthariq <thoriq.noer@gmail.com>
2020-06-22 07:36:20 +0700

    Tambah menu product-galleries

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -17,7 +17,7 @@
 
                 <li class="menu-title">Foto Barang</li><!-- /.menu-title -->
                 <li class="">
-                    <a href="#"> <i class="menu-icon fa fa-list"></i>Lihat Foto Barang</a>
+                <a href="{{ route('product-galleries.index') }}"> <i class="menu-icon fa fa-list"></i>Lihat Foto Barang</a>
                 </li>
                 <li class="">
                     <a href="#"> <i class="menu-icon fa fa-plus"></i>Tambah Foto Barang</a>
