fe07040c6a8ef4eb778ab6dc14cfd4511ca8d9ee
Nurmuhammadthariq <thoriq.noer@gmail.com>
2020-06-22 07:39:42 +0700

    First Commit Index Galleries

---
+++ resources/views/pages/product-galleries/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.default')
+
+@section('content')
+<div class="orders">
+	<div class="row">
+			<div class="col-12">
+				<div class="card">
+					<div class="card-body">
+						<h4 class="box-title">Daftar Barang</h4>
+					</div>
+					<div class="card-body--">
+							<div class="table-stats order-table ov-h">
+									<table class="table ">
+											<thead>
+													<tr>
+															<th>No</th>
+															<th>Nama Product</th>
+															<th>Foto</th>
+															<th>Default</th>
+															<th>Action</th>
+													</tr>
+											</thead>
+											<tbody>
+											
+											</tbody>
+									</table>
+							</div> <!-- /.table-stats -->
+					</div>
+				</div> <!-- /.card -->
+			</div>  <!-- /.col-lg-8 -->
+	</div>
+</div>
+@endsection
